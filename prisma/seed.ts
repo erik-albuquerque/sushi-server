5ec3c87397fd63cb02e43601570c43ab59e4ff4a
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2022-11-18 22:49:47 -0300

    feat(database): add seeds to the database

---
+++ prisma/seed.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const main = async () => {
+	const user = await prisma.user.create({
+		data: {
+			name: 'John DOe',
+			email: 'johndoe@gmail.com',
+			// avatarUrl: '',
+			password: '12345678',
+			ownRoles: {
+				create: {
+					title: 'user.hot-roll'
+				}
+			}
+		}
+	})
+
+	await prisma.room.create({
+		data: {
+			title: 'global',
+			ownerId: user.id,
+			trackQueue: {
+				create: {
+					tracks: {
+						create: {
+							title: 'Chamber of Reflection - Mac DeMarco (Live Cover)',
+							url: 'https://www.youtube.com/watch?v=l0NjZLmkODA',
+							ownerId: user.id
+						}
+					}
+				}
+			},
+			participants: {
+				create: {
+					userId: user.id
+				}
+			}
+		}
+	})
+}
+
+main()
